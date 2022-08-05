53b676a5a4d5b7c0c3746754f99af578fa1f682d
Diel01 <98334604+Diel01@users.noreply.github.com>
2022-08-05 05:19:13 +0100

    Add files via upload

---
+++ 2mfossept/index.php
@@ -0,0 +1,89 @@
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="2mfossept.css">
+   
+   <title>2mfossept</title>
+</head>
+<body>
+   <div>
+   <section>
+      <strong>2MFOSSEPT</strong><br><br><br>
+      <img src="imges/3.jpg" alt=""><br><br><br>
+   </section>
+
+   <section class="presentation">
+      <img src="imges/2.jpg" alt="">
+      <P>
+         Notre entreprise,
+            <strong>
+               2mfossept
+            </strong> 
+               assure l'instalation de
+             <strong>
+                fosses septique au Gabon.
+             </strong>
+         Nous sommes à votre disposition pour vous conseiller. 
+            </P>
+   </section><br><br><br>
+   <section class="suite">
+      <img src="imges/3.jpg" alt="">
+      <P>
+         Confiez tous vos projets d'instalation de 
+            <strong>
+               fosses septique
+            </strong>
+         autour de 
+            <strong>
+               Libreville à notre entreprise. Chez 2mfossept, 
+               nous vous conseillons sur le type d'équipement 
+               le plus adapté à vos besoin et nous assurons une 
+               instalation rapide de A à Z.
+            </strong>
+      </P>
+
+   </section><br><br><br>
+
+   <section class="nosPrestation">
+      <h1>Nos prestation</h1>
+      <img src="imges/3.jpg" alt="">
+         <p>
+            <strong>Spécialiste</strong>
+            des questions de terrassement, nous sommes compétents, 
+            expérimentés et ponctuel pour réaliser l'installation 
+            de A à Z de vos systèmes 
+            <strong>
+               d'assainissement.
+            </strong>
+            Nous intervenons notamment sur l'installation de
+            <strong>
+               fosses septique
+            </strong>
+         </p>
+
+      <h2>
+         Les étapes de l'instalation
+      </h2>
+      
+   </section><br><br><br>
+</div> 
+<div class="gal">
+   <img src="imges/3.jpg" alt="">
+   <img src="imges/3.jpg" alt="">
+   <img src="imges/3.jpg" alt="">
+   <img src="imges/3.jpg" alt="">
+   <img src="imges/3.jpg" alt="">
+   <img src="imges/3.jpg" alt="">
+</div>  
+<h3>NOUS CONTACTER</h3>
+<section class="contact">
+   <p>Adresse <span>diel</span></p>
+   <P>Téléphone <span>77-537-24-52</span></P>
+   <p>Ouverture <span>700</span></p>  
+</section>
+
+</body>
+<script src="2mfossept.js"></script>
+</html>
